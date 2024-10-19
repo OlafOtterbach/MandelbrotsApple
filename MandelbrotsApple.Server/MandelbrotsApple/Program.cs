d2dccce3e9c8f961030d68d37f43412bffae3079
Olaf Otterbach <olaf.otterbach@gmx.de>
2024-10-19 08:46:05 +0200

    Adding cors configuration.

--- MandelbrotsApple.Server/MandelbrotsApple/Program.cs
+++ MandelbrotsApple.Server/MandelbrotsApple/Program.cs
@@ -2,7 +2,20 @@ using MandelbrotsApple.Mandelbrot.Model;
 using static MandelbrotsApple.Mandelbrot.MandelbrotSetGenerator;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapGet("/", () => "Hello Mandelbrot!");
 app.MapPost("/mandelbrot", (MandelbrotParameter request) => Results.Ok(GenerateMandelbrotSet(request)));
