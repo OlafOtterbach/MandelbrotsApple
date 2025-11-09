53ce2b538fabbf5ff15fbe2208d8cc3c020d0578
Olaf Otterbach <olaf.otterbach@gmx.de>
2025-11-09 14:07:48 +0100

    Adding functions for mandelbrot calculation.

--- MandelbrotsApple/EventConverter.cs
+++ MandelbrotsApple/EventConverter.cs
@@ -1,10 +1,33 @@
-﻿using MandelbrotsApple.Mandelbrot;
+﻿namespace MandelbrotsApple;
 
-namespace MandelbrotsApple;
+using MandelbrotsApple.Mandelbrot;
+using static MandelbrotsApple.Mandelbrot.View;
 
 public static class EventConverter
 {
-    public static MandelbrotParameter Convert(this Init init, MandelbrotState state)
+    public static Func<MandelbrotState, MandelbrotResult> CreateInit(this Init init)
+        => state => View.Initialize(init.InitToParameter(state));
+
+    public static Func<MandelbrotState, MandelbrotResult> CreateMaxIteration(this MaxIteration maxIteration)
+        => state => View.Refresh(maxIteration.MaxIterationToParameter(state));
+
+    public static Func<MandelbrotState, MandelbrotResult> CreateRefresh(this Refresh refresh)
+        => state => View.Refresh(refresh.RefreshToParameter(state));
+
+    public static Func<MandelbrotState, MandelbrotResult> CreateMove(this Move move)
+        => state => View.Move(move.MoveToMoveParameter(state));
+
+    public static Func<MandelbrotState, MandelbrotResult> CreateRefresh(this Move move)
+        => state => View.Refresh(move.MoveToParameter(state));
+
+    public static Func<MandelbrotState, MandelbrotResult> CreateZoom(this Zoom zoom)
+        => state => View.Zoom(zoom.ZoomToZoomParameter(state));
+
+
+
+
+
+    private static MandelbrotParameter InitToParameter(this Init init, MandelbrotState state)
     {
         var maxIterationValue = GetMaxIteration(init.IterationPercentage);
         return new MandelbrotParameter(
@@ -13,7 +36,7 @@ public static class EventConverter
             MaxIterations: maxIterationValue);
     }
 
-    public static MandelbrotParameter Convert(this MaxIteration maxIteration, MandelbrotState state)
+    private static MandelbrotParameter MaxIterationToParameter(this MaxIteration maxIteration, MandelbrotState state)
     {
         var maxIterationValue = GetMaxIteration(maxIteration.IterationPercentage);
         return new MandelbrotParameter(
@@ -22,7 +45,7 @@ public static class EventConverter
             MaxIterations: maxIterationValue);
     }
 
-    public static MandelbrotParameter Convert(this Refresh refresh, MandelbrotState state)
+    private static MandelbrotParameter RefreshToParameter(this Refresh refresh, MandelbrotState state)
     {
         return new MandelbrotParameter(
             ImageSize: refresh.ImageSize,
@@ -30,7 +53,15 @@ public static class EventConverter
             MaxIterations: state.MaxIterations);
     }
 
-    public static MandelbrotMoveParameter Convert(this Move move, MandelbrotState state)
+    private static MandelbrotParameter MoveToParameter(this Move move, MandelbrotState state)
+    {
+        return new MandelbrotParameter(
+            ImageSize: move.ImageSize,
+            CurrentMandelbrotSize: state.Size,
+            MaxIterations: state.MaxIterations);
+    }
+
+    private static MandelbrotMoveParameter MoveToMoveParameter(this Move move, MandelbrotState state)
     {
         return new MandelbrotMoveParameter(
            ImageMoveVector: move.ImageMoveVector,
@@ -39,7 +70,7 @@ public static class EventConverter
            MaxIterations: state.MaxIterations);
     }
 
-    public static MandelbrotZoomParameter Convert(this Zoom zoom, MandelbrotState state)
+    private static MandelbrotZoomParameter ZoomToZoomParameter(this Zoom zoom, MandelbrotState state)
     {
         return new MandelbrotZoomParameter(
             MousePosition: zoom.ImagePosition,
